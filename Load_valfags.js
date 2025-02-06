b4c2b87e7e4f24d7091bd717ac9de0d8ec89cf7e
Tepz <Tepz@Tepz.com>
2025-02-06 20:42:05 +0100

    Update

---
+++ Load_valfags.js
@@ -0,0 +1,15 @@
+const Alle_Valgfag = ["Musik", "Gaming", "Film"]
+
+const valgfags = document.getElementsByClassName("Valgfags");
+
+for (let i=0;i<Alle_Valgfag.length;i++) {
+	const valfag = document.createElement("div")
+	valfag.className = "valgfag"
+	valgfags[0].appendChild(valfag)
+
+	const valgfag_text = document.createElement("p")
+	valgfag_text.style = "color: #ffffff; font-size: 50px"
+	valgfag_text.textContent = Alle_Valgfag[i]
+
+	valfag.appendChild(valgfag_text)
+}
